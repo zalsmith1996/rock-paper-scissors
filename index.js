efec1202fef3cc623c0accaca14f2efce6bc24ab
Zack <zalsmith1996@gmail.com>
2023-12-24 23:46:50 -0500

    Update game logic

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Enter Rock, Paper, or Scissors: ");
-playerSelection.toLowerCase();
+let playerWins = 0;
+let computerWins = 0;
 
-playRound(playerSelection, computerSelection);
+game(); 
 
 // Return computer's randomized choice of rock, paper, scissors
-function getComputerChoice () {
+function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
     let choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
 
-function capitalizeString (string) {
+function capitalizeString(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
 function playRound(playerSelection, computerSelection) {
+    
     if (playerSelection === computerSelection)
     {
-        console.log(`It's a tie! Restarting round.`);
-        const computerSelection = getComputerChoice();
-        computerSelection.toLowerCase();
-        const playerSelection = prompt("Enter Rock, Paper, or Scissors: ");
-        playerSelection.toLowerCase();
-        playRound(playerSelection, computerSelection);
+        console.log(`It's a tie! Scores remain unchanged.`);
     }
     else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
@@ -31,8 +26,31 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'paper' && computerSelection === 'rock')
     ) {
         console.log(`You win! ${capitalizeString(playerSelection)} beats ${capitalizeString(computerSelection)}.`);
+        playerWins++;
     }
     else {
         console.log(`You lose! ${capitalizeString(computerSelection)} beats ${capitalizeString(playerSelection)}.`);
+        computerWins++;
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("Enter Rock, Paper, or Scissors: ");
+        playerSelection.toLowerCase();
+
+        playRound(playerSelection, computerSelection);
+    }
+
+    if (playerWins === computerWins) {
+        console.log("After 5 rounds, it's a tie!");
+    }
+    else if
+        (playerWins > computerWins) {
+            console.lose("After 5 rounds, you win!");
+    }
+    else {
+        console.log("After 5 rounds, you lose!");
     }
 }
